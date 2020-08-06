639330b25bb9b3144168324ef0aad095502dfbf9
sdanyalk <s.danyal.k@gmail.com>
2020-08-06 15:06:38 -0700

    initial commit

---
+++ src/main/java/com/sdk/securedsuperheroes/SecuredSuperheroesApplication.java
@@ -0,0 +1,13 @@
+package com.sdk.securedsuperheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredSuperheroesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuredSuperheroesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdk/securedsuperheroes/SecuredSuperheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdk.securedsuperheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredSuperheroesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
